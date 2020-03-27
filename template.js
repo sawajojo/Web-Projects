a7b48286eae35a935287b7a7322b295d4181bb46
sawajojo <60889399+sawajojo@users.noreply.github.com>
2020-03-26 20:15:27 -0400

    Update

--- template.js
+++ template.js
@@ -1,19 +1,5 @@
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
- // When the user scrolls the page, execute myFunction
-
-
-/*
-function fixedClassChange() {
-
-    if (window.pageYOffset > 100) {
-     header.classList.add("sticky");
-     sidebar.classList.add("stickySide");
-    } else {
-    header.classList.remove("sticky");
-    sidebar.classList.remove("stickySide");
-     }
-}
-*/
+      
+     
 
 
